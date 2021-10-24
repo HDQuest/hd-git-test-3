5959b17aba157dd69c382e45753381d40faed800
HDQuest <hilmidc04@gmail.com>
2021-10-24 17:50:03 +0700

    Membuat file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Remote 3 (Ubah Nama)</title>
+</head>
+<body>
+    <h1>Salam dari Binjai!</h1>
+    <p>Ini percobaan remote dengan mengganti nama (ikut intruksi dari Github).</p>
+    
+</body>
+</html>
